a07a561a014f384932b85719f171b6816cb51764
rushidholecom <dholerushi.com@gmail.com>
2026-02-18 16:40:15 +0530

    fix

--- s3-static-website/main.tf
+++ s3-static-website/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "website_bucket" {
-  bucket = var.website_bucket
+  bucket = var.bucket_name
 }
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
