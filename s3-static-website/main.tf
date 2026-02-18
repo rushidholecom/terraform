bee298f099c1b7dd7478cdf17eadfa8ea1b93937
rushidholecom <dholerushi.com@gmail.com>
2026-02-18 16:32:03 +0530

    host static website

---
+++ s3-static-website/main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.website_bucket
+}
+
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.public_access
+  ]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_object" "index" {
+  bucket       = aws_s3_bucket.website_bucket.id
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
---
+++ s3-static-website/variable.tf
@@ -0,0 +1,3 @@
+variable "bucket_name" {
+  default = "rushi-static-site-terraform-2026"
+}
