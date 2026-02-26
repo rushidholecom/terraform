be0c8c4c364a94e776d5c71eb2808e21f41e4d01
rushidholecom <dholerushi.com@gmail.com>
2026-02-26 17:43:36 +0530

    create target group

---
+++ loadbalance-autosacling/load-balancer.tf
@@ -0,0 +1,38 @@
+resource "aws_lb_target_group" "home_target_group" {
+  name = "${var.project}-${var.env}-home-tg"
+  port = 80
+  protocol = "http"
+  vpc_id = data.aws_vpc.default
+  health_check {
+    enabled = true
+    path = "/"
+    port = "traffic-port"
+    protocol = "HTTP"
+  }
+}
+
+resource "aws_lb_target_group" "mobile_target_group" {
+  name = "${var.project}-${var.env}-mobile-tg"
+  port = 80
+  protocol = "http"
+  vpc_id = data.aws_vpc.default
+  health_check {
+    enabled = true
+    path = "/mobile"
+    port = "traffic-port"
+    protocol = "HTTP"
+  }
+}
+
+resource "aws_lb_target_group" "laptop_target_group" {
+  name = "${var.project}-${var.env}-laptop-tg"
+  port = 80
+  protocol = "http"
+  vpc_id = data.aws_vpc.default
+  health_check {
+    enabled = true
+    path = "/laptop"
+    port = "traffic-port"
+    protocol = "HTTP"
+  }
+}
