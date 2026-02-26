d26e2ae8fb16e321344ab1e13fd18a7e008efe6e
rushidholecom <dholerushi.com@gmail.com>
2026-02-26 18:58:47 +0530

    fix

--- loadbalance-autosacling/load-balancer.tf
+++ loadbalance-autosacling/load-balancer.tf
@@ -2,7 +2,7 @@ resource "aws_lb_target_group" "home_target_group" {
   name = "${var.project}-${var.env}-home-tg"
   port = 80
   protocol = "HTTP"
-  vpc_id = data.aws_vpc.default
+  vpc_id = data.aws_vpc.default.id
   health_check {
     enabled = true
     path = "/"
@@ -15,7 +15,7 @@ resource "aws_lb_target_group" "mobile_target_group" {
   name = "${var.project}-${var.env}-mobile-tg"
   port = 80
   protocol = "HTTP"
-  vpc_id = data.aws_vpc.default
+  vpc_id = data.aws_vpc.default.id
   health_check {
     enabled = true
     path = "/mobile"
@@ -28,7 +28,7 @@ resource "aws_lb_target_group" "laptop_target_group" {
   name = "${var.project}-${var.env}-laptop-tg"
   port = 80
   protocol = "HTTP"
-  vpc_id = data.aws_vpc.default
+  vpc_id = data.aws_vpc.default.id
   health_check {
     enabled = true
     path = "/laptop"
