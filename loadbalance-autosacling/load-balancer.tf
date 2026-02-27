a21996b428d63d05b71dd81a989698fd63a600fe
rushidholecom <dholerushi.com@gmail.com>
2026-02-27 17:32:10 +0530

    create listener

--- loadbalance-autosacling/load-balancer.tf
+++ loadbalance-autosacling/load-balancer.tf
@@ -14,3 +14,13 @@ resource "aws_lb" "lb" {
   internal = false
   subnets = data.aws_subnets.subnet-1.ids
 }
+
+resource "aws_lb_listener" "lb_listener" {
+  load_balancer_arn = aws_lb.lb.arn
+  port = 80
+  protocol = "HTTP"
+  default_action {
+    type = "forword"
+    target_group_arn = aws_lb_target_group.home_target_group.arn
+  }
+}
